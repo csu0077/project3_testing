684f3445b8c55ab2f076f453e925175ce9197e6b
Clayton Su <csu007@ucr.edu>
2018-02-19 23:58:11 -0500

    can't fix webdriver bug looking in the wrong folder, just pushing with duct tape fix

--- src/main/java/com/revature/test/utils/WebDriverUtil.java
+++ src/main/java/com/revature/test/utils/WebDriverUtil.java
@@ -16,8 +16,14 @@ public class WebDriverUtil {
 	public static WebDriver getChromeDriver() {
 		if (chromeDriver == null) {
 			//doesnt work, for some reason i have to put driver in \sts-3.9.1-RELEASE\src\main\resources
-			//like the actual installation of sts
-			File f1 = new File(".\\src\\main\\resources\\chromedriver.exe");
+			//some path like that
+			//like the actual installation of sts, if you get a testNG error, check the stack trace
+			// for the directory this code is looking into
+			//if you want to see what happening on the ec2-54-210-36-233.compute-1 instance
+			//you can check the logs folder in the apache installation
+			// currently there is a chromedriver in the bin/src/main/resources of the Apache folder
+			// ---God Save the Queen
+			File f1 = new File("src/main/resources/chromedriver.exe");
 			System.setProperty("webdriver.chrome.driver", f1.getAbsolutePath());
 			return new ChromeDriver();
 		}
